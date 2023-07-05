03fe1e80ec5fa0598959a37329f33d9b3757c858
M-Ankitha <122814756+M-Ankitha@users.noreply.github.com>
2023-07-05 09:11:27 +0530

    Update MessageCard.jsx

--- src/components/MessageCard.jsx
+++ src/components/MessageCard.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { useSelector } from "react-redux";
+import  { useState} from "react";
 
 export default function MessageCard({ currentToUser }) {
   //message list data are taken from store
   const messageList = useSelector((state) => state);
-
+  const [like, setLike] = useState(0),
+  [isLike, setIsLike] = useState(false),
+  onLikeButtonClick=()=>{
+    setLike(like + (isLike ? 1:1));
+    setIsLike(!isLike);
+  }
   return (
     <div>
       <div>
+      
         {/**first message list is sorted by time 
   then only message that are between from user(Ankitha) and to user is taken out and then its displayed*/}
         {messageList
@@ -36,15 +43,18 @@ export default function MessageCard({ currentToUser }) {
                     : { alignItems: "flex-start" }
                 }
               >
+                
                 <Card
                   border="secondary"
                   style={{
                     width: "18rem",
                     marginRight: "50px",
                     marginLeft: "50px",
+                   
                   }}
                   className="mb-3 "
                 >
+                
                   <Card.Body>
                     <Card.Text>{contact.message}</Card.Text>
                     <Card.Footer>
@@ -55,6 +65,14 @@ export default function MessageCard({ currentToUser }) {
                     </Card.Footer>
                   </Card.Body>
                 </Card>
+                <div className="like">
+                   <marginRight>
+                    <p className={""+ (isLike ? "text-danger":"text-danger")}>
+                    <i class ="fa fa-heart" onClick={onLikeButtonClick}></i>
+                    <span className="=text-danger">{like}</span>
+                    </p>
+                    </marginRight>
+                  </div> 
               </div>
             );
           })}
